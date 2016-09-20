0ae95ac3659d0055f24735fb90522596a2b5c494
Edson Elmar Schlei <edson.schlei@wabion.com>
2016-09-20 15:54:26 +0200

    Using the projectDir not the rootDir
    - when the subproject is another checkout from other project the version is readed wrong

--- src/main/groovy/com/palantir/gradle/gitversion/GitVersionPlugin.groovy
+++ src/main/groovy/com/palantir/gradle/gitversion/GitVersionPlugin.groovy
@@ -32,7 +32,7 @@ class GitVersionPlugin implements Plugin<Project> {
 
     @Memoized
     private File gitDir(Project project) {
-        return getRootGitDir(project.rootDir)
+        return getRootGitDir(project.projectDir)
     }
 
     @Memoized
