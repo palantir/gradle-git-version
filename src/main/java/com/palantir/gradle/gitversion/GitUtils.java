ea10a730df2f885db2a66321aa0f82146ed405d7
Dan Fox <dfox@palantir.com>
2018-04-20 15:25:29 +0100

    Move util closer to usage

--- src/main/java/com/palantir/gradle/gitversion/GitUtils.java
+++ src/main/java/com/palantir/gradle/gitversion/GitUtils.java
@@ -1,8 +1,5 @@
 package com.palantir.gradle.gitversion;
 
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
-
 class GitUtils {
 
     static final int SHA_ABBR_LENGTH = 7;
@@ -11,14 +8,5 @@ class GitUtils {
 
     static String abbrevHash(String s) {
         return s.substring(0, SHA_ABBR_LENGTH);
-    }
-
-    // getPeeledObjectId returns:
-    // "if this ref is an annotated tag the id of the commit (or tree or blob) that the annotated tag refers to;
-    // null if this ref does not refer to an annotated tag."
-    // We use this to check if tag is annotated.
-    static boolean isAnnotatedTag(Ref ref) {
-        ObjectId peeledObjectId = ref.getPeeledObjectId();
-        return peeledObjectId != null;
     }
 }
