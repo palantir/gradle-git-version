b5847d23d5cb9088a47da0d55b2e29b04f27da2f
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2019-08-07 12:09:24 +0100

    Revert changes after 0.11.0 (#150)
    Revert code back to what was in 0.11.0.

--- src/main/java/com/palantir/gradle/gitversion/VersionDetails.java
+++ src/main/java/com/palantir/gradle/gitversion/VersionDetails.java
@@ -59,7 +59,23 @@ public final class VersionDetails {
             return null;
         }
 
-        return new JGitDescribe(git).describe(args.getPrefix());
+        String nativeGitDescribe = new NativeGitDescribe(git.getRepository().getDirectory())
+                .describe(args.getPrefix());
+        String jgitDescribe = new JGitDescribe(git)
+                .describe(args.getPrefix());
+
+        // If native failed, return JGit one
+        if (nativeGitDescribe == null) {
+            return jgitDescribe;
+        }
+
+        // If native succeeded, make sure it's same as JGit one
+        if (!nativeGitDescribe.equals(jgitDescribe)) {
+            throw new IllegalStateException(String.format("Inconsistent git describe: native was %s and jgit was %s. " +
+                    "Please report this on github.com/palantir/gradle-git-version", nativeGitDescribe, jgitDescribe));
+        }
+
+        return jgitDescribe;
     }
 
     private boolean isRepoEmpty() {
