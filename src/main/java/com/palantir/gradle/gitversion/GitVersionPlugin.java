92c32e8122c7a75c8dd1141522c02cfcbe6845cd
Felipe Orozco <ferozco@users.noreply.github.com>
2019-09-03 23:47:37 -0400

    Apply baseline (#158)
    * apply baseline
    
    * remove magic plugin

--- src/main/java/com/palantir/gradle/gitversion/GitVersionPlugin.java
+++ src/main/java/com/palantir/gradle/gitversion/GitVersionPlugin.java
@@ -1,6 +1,24 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.palantir.gradle.gitversion;
 
 import groovy.lang.Closure;
+import java.io.File;
+import java.io.IOException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.gradle.api.Action;
@@ -8,10 +26,8 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 
-import java.io.File;
-import java.io.IOException;
-
-public class GitVersionPlugin implements Plugin<Project> {
+public final class GitVersionPlugin implements Plugin<Project> {
+    @Override
     public void apply(final Project project) {
 
         final Git git = gitRepo(project);
@@ -32,6 +48,7 @@ public class GitVersionPlugin implements Plugin<Project> {
         Task printVersionTask = project.getTasks().create("printVersion");
         printVersionTask.doLast(new Action<Task>() {
             @Override
+            @SuppressWarnings("BanSystemOut")
             public void execute(Task task) {
                 System.out.println(project.getVersion());
             }
@@ -73,4 +90,3 @@ public class GitVersionPlugin implements Plugin<Project> {
         return scanForRootGitDir(currentRoot.getParentFile());
     }
 }
-
--- src/main/java/com/palantir/gradle/gitversion/NativeGitDescribe.java
+++ src/main/java/com/palantir/gradle/gitversion/NativeGitDescribe.java
@@ -1,22 +1,38 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.palantir.gradle.gitversion;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Sets;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Mimics git describe by using rev-list to support versions of git < 1.8.4
+ * Mimics git describe by using rev-list to support versions of git < 1.8.4.
  */
 class NativeGitDescribe implements GitDescribe {
     private static final Logger log = LoggerFactory.getLogger(NativeGitDescribe.class);
@@ -39,7 +55,8 @@ class NativeGitDescribe implements GitDescribe {
         pb.redirectErrorStream(true);
 
         Process process = pb.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
 
         StringBuilder builder = new StringBuilder();
         String line = null;
@@ -77,8 +94,9 @@ class NativeGitDescribe implements GitDescribe {
                 if (tagRefs.contains(rev)) {
                     String exactTag = runGitCmd("describe", "--tags", "--exact-match", "--match=" + prefix + "*", rev);
                     if (!exactTag.isEmpty()) {
-                        return depth == 0 ?
-                                exactTag : String.format("%s-%s-g%s", exactTag, depth, GitUtils.abbrevHash(revs.get(0)));
+                        return depth == 0
+                                ? exactTag
+                                : String.format("%s-%s-g%s", exactTag, depth, GitUtils.abbrevHash(revs.get(0)));
                     }
                 }
             }
