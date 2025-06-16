a15105e7d92b24b04e645d48118be7041ef8ebb5
Kelvin Ou <kelvin.ou.jin.bin@gmail.com>
2025-06-16 13:26:50 +0100

    Test

--- src/main/java/com/palantir/gradle/gitversion/GitVersionCacheService.java
+++ src/main/java/com/palantir/gradle/gitversion/GitVersionCacheService.java
@@ -15,15 +15,16 @@
  */
 package com.palantir.gradle.gitversion;
 
-import java.io.File;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import javax.inject.Inject;
 import org.gradle.api.Project;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.services.BuildService;
 import org.gradle.api.services.BuildServiceParameters;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public abstract class GitVersionCacheService implements BuildService<BuildServiceParameters.None> {
 
@@ -40,7 +41,6 @@ public abstract class GitVersionCacheService implements BuildService<BuildServic
         File gitDir = getRootGitDir(project);
         GitVersionArgs gitVersionArgs = GitVersionArgs.fromGroovyClosure(args);
         String key = gitDir.toPath() + "|" + gitVersionArgs.getPrefix();
-
         return versionDetailsMap.computeIfAbsent(
                 key, _k -> new VersionDetailsImpl(getProviderFactory(), gitDir, gitVersionArgs));
     }
@@ -72,6 +72,7 @@ public abstract class GitVersionCacheService implements BuildService<BuildServic
     public static Provider<GitVersionCacheService> getSharedGitVersionCacheService(Project project) {
         return project.getGradle()
                 .getSharedServices()
-                .registerIfAbsent("GitVersionCacheService", GitVersionCacheService.class, _spec -> {});
+                .registerIfAbsent("GitVersionCacheService", GitVersionCacheService.class, _spec -> {
+                });
     }
 }
